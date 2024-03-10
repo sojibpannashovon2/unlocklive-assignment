3c2c3985b8c5b3584640e19609941e5847a34a4d
Md Arifur Rahman <sojibkhanarif999@gmail.com>
2024-03-10 21:46:13 +0600

    readme file add for documentation

--- client/src/componet/Shared/Container.jsx
+++ client/src/componet/Shared/Container.jsx
@@ -2,6 +2,7 @@ import React from "react";
 
 const Container = ({ children }) => {
   return (
+    // zczx
     <>
       <div className="max-w-[1920px] mx-auto xl:px-20 md:px-10 sm:px-2 px-4">
         {children}
